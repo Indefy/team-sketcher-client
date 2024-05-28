4edbcc4e380bbee2e7953a7e7baffdc6f5a03396
Adi Booker <adib4324@gmail.com>
2024-05-28 15:04:50 +0300

    Updated Whiteboard Context Extended State Management

--- src/context/WhiteboardContext.jsx
+++ src/context/WhiteboardContext.jsx
@@ -8,6 +8,10 @@ export const WhiteboardProvider = ({ children }) => {
 	const [brushSize, setBrushSize] = useState(5);
 	const [brushType, setBrushType] = useState("default");
 	const [color, setColor] = useState("black");
+	const [canvasHistory, setCanvasHistory] = useState([]);
+	const [historyIndex, setHistoryIndex] = useState(-1);
+	const [isDrawing, setIsDrawing] = useState(false);
+	const [users, setUsers] = useState([]);
 
 	return (
 		<WhiteboardContext.Provider
@@ -18,9 +22,44 @@ export const WhiteboardProvider = ({ children }) => {
 				setBrushType,
 				color,
 				setColor,
+				canvasHistory,
+				setCanvasHistory,
+				historyIndex,
+				setHistoryIndex,
+				isDrawing,
+				setIsDrawing,
+				users,
+				setUsers,
 			}}
 		>
 			{children}
 		</WhiteboardContext.Provider>
 	);
 };
+
+// import React, { createContext, useContext, useState } from "react";
+
+// const WhiteboardContext = createContext();
+
+// export const useWhiteboardContext = () => useContext(WhiteboardContext);
+
+// export const WhiteboardProvider = ({ children }) => {
+// 	const [brushSize, setBrushSize] = useState(5);
+// 	const [brushType, setBrushType] = useState("default");
+// 	const [color, setColor] = useState("black");
+
+// 	return (
+// 		<WhiteboardContext.Provider
+// 			value={{
+// 				brushSize,
+// 				setBrushSize,
+// 				brushType,
+// 				setBrushType,
+// 				color,
+// 				setColor,
+// 			}}
+// 		>
+// 			{children}
+// 		</WhiteboardContext.Provider>
+// 	);
+// };
